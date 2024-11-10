accd150e462fe5bf7da77cd94d5140d7e7b63e6c
AlekOmOm <142972647+AlekOmOm@users.noreply.github.com>
2024-11-10 17:05:41 +0100

    init

---
+++ src/main/java/com/alek0m0m/aicookbookapiclient/AiCookBookApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.alek0m0m.aicookbookapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCookBookApiClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCookBookApiClientApplication.class, args);
+    }
+
+}
